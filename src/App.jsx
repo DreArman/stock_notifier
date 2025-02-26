b9cdce88cfb9d7b96c6f4ef4281e8d7944e81561
damirbeybitov <damirbey7@gmail.com>
2025-02-27 00:22:01 +0500

    Refactor routing and authentication flow; add login and registration pages, and move Home page to a new directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Headers from "./components/Headers"; 
+import Home from "./pages/main/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import { isAuthenticated } from "./utils/auth.js";
 
 // Define an enum for the pages
 const Pages = {
   ROOT: "/",
   HOME: "/home",
+  LOGIN: "/login",
+  REGISTER: "/register",
 };
 
 function App() {
+  if (isAuthenticated) {
+    return (
+      <Router>
+        <Routes>
+          <Route path={Pages.ROOT} element={<Home />} />
+          <Route path={Pages.HOME} element={<Home />} />
+          <Route path={Pages.LOGIN} element={<Login />} />
+          <Route path={Pages.REGISTER} element={<Register />} />
+        </Routes>
+      </Router>
+    )
+  }
   return (
     <Router>
-      <Headers />
       <Routes>
-        <Route path={Pages.ROOT} element={<Home />} />
-        <Route path={Pages.HOME} element={<Home />} />
+        <Route path={Pages.ROOT} element={<Login />} />
+        <Route path={Pages.LOGIN} element={<Login />} />
+        <Route path={Pages.REGISTER} element={<Register />} />
       </Routes>
     </Router>
   );
