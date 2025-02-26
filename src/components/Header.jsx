3e21332a1fbe3221f1885b34a8fffe3b2795bf03
DreArman <mdream6904@gmail.com>
2025-02-27 01:30:01 +0500

    Update Header component navigation and restructure index.html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,18 @@ const Header = () => {
       </a>
 
       <ul className="nav nav-pills">
-        <li className="nav-item"><a href="/home" className="nav-link active" aria-current="page">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
-        <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle"></img>
-          </a>
+        <li className="nav-item"><a href="/home" className="nav-link active" aria-current="page">Main</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Stocks</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Predict</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Alerts</a></li>
       </ul>
-    </header>
+      <a href="#" className="nav-link text">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi d-block mx-auto mb-1" viewBox="0 0 16 16">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+          <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+        </svg>
+      </a>
+  </header>
   </div>
   );
 };
