392dffec94a40602206b4216b35e9b49f9de6de5
damirbeybitov <damirbey7@gmail.com>
2025-05-01 03:12:13 +0500

    Refactor stock service functions for improved data fetching; remove unused authentication utility

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,14 +1,35 @@
 import API from "./axiosInstance";
 
-export const getStockData = async () => {
+
+export const getStocTickers = async () => {
   try {
-    const response = await API.get("/stocks");
+    const response = await API.get("/tickers");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching stock tickers:", error);
+    throw error;
+  }
+}
+
+export const getUserStocks = async () => {
+  try {
+    const response = await API.get("/user-stock");
     return response.data;
   } catch (error) {
     console.error("Error fetching stock data:", error);
     throw error;
   }
 };
+
+export const getStockData = async (stock_tickers) => {
+  try {
+    const response = await API.get("/stocks", { tickers: stock_tickers });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching stock data:", error);
+    throw error;
+  }
+}
 
 export const addStock = async (stock_ticker, type) => {
   try {
@@ -19,3 +40,13 @@ export const addStock = async (stock_ticker, type) => {
     throw error;
   }
 };
+
+export const deleteStock = async (stock_ticker) => {
+  try {
+    const response = await API.delete(`/user-stock/${stock_ticker}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting stock:", error);
+    throw error;
+  }
+}
